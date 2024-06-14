598bfeca91cc5a97d116b8fb82e60e86b2e13427
sorutrt <kokishiono.58@gmail.com>
2024-06-14 21:13:12 +0900

    square answer 1

--- square.cpp
+++ square.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Square {
+private:
+    int position_x, position_y;
+    int width, height;
+public:
+    // constructor
+    Square(int px, int py, int w, int h) {
+        position_x = px;
+        position_y = py;
+        width = w;
+        height = h;
+    }
+
+    // getter
+    int getPx() { return position_x; }
+    int getPy() { return position_y; }
+    int getWidth() { return width; }
+    int getHeight() { return height; }
+};
+
 
 int main() {
 
